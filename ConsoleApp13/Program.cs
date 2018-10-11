f67aefef3f754872511986bff6172e28c0336264
César Tapia <c.tapia@korvi.net>
2018-10-11 12:02:24 -0500

    Finished dictionary

--- ConsoleApp13/Program.cs
+++ ConsoleApp13/Program.cs
@@ -8,8 +8,46 @@ namespace ConsoleApp13
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            Customr customer1 = new Customr()
+            {
+                ID = 101,
+                Name = "Cesar",
+                Salary = 5000
+            };
+
+            Customr customer2 = new Customr()
+            {
+                ID = 110,
+                Name = "Pam",
+                Salary = 5000
+            };
+
+            Customr customer3 = new Customr()
+            {
+                ID = 119,
+                Name = "John",
+                Salary = 6500
+            };
+
+            Dictionary<int, Customr> dictionaryCustomers = new Dictionary<int, Customr>();
+            dictionaryCustomers.Add(customer1.ID, customer1);
+            dictionaryCustomers.Add(customer2.ID, customer2);
+            dictionaryCustomers.Add(customer3.ID, customer3);
+
+            if (dictionaryCustomers.ContainsKey(135))
+            {
+                Customr cust = dictionaryCustomers[135];
+            }
+            Console.ReadKey();
         }
+    }
+
+    public class Customr
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
     }
 }
